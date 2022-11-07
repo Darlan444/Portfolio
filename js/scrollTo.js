ccb13432a2098aa3a55e756eede7cbf3f96dfe68
Darlan444 <darlaninacio321@gmail.com>
2022-11-07 03:38:59 -0300

    scrollTo Menu

---
+++ js/scrollTo.js
@@ -0,0 +1,12 @@
+function scrollToHome(){
+    window.scrollTo( 0, 0 );
+}
+function scrollToSobre(){
+    window.scrollTo( 0, 947 );
+}
+function scrollToProjetos(){
+    window.scrollTo( 0, 1855 );
+}
+function scrollToContato(){
+    window.scrollTo( 0, 0 );
+}

e5ec8745d489568a6e39b9422d143b5693a19180
Darlan444 <darlaninacio321@gmail.com>
2022-11-17 03:29:23 -0300

    js/mob

---
+++ js/menu.js
@@ -0,0 +1,7 @@
+function classMenu() {
+    let el = document.getElementById('menuClass');
+    el.classList.add('active');
+
+    let icon = document.getElementById('icon-menu')
+    icon.style.visibility="hidden";
+}
---
+++ js/scrollToMob.js
@@ -0,0 +1,12 @@
+function scrollToHomeMob(){
+    window.scrollTo( 0, 0 );
+}
+function scrollToSobreMob(){
+    window.scrollTo( 0, 807 );
+}
+function scrollToProjetosMob(){
+    window.scrollTo( 0, 1830 );
+}
+function scrollToContatoMob(){
+    window.scrollTo( 0, 5400 );
+}

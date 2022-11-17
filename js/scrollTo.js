1c15979682f7c1123049dbfde2c99e832c5db320
Darlan444 <darlaninacio321@gmail.com>
2022-11-17 01:39:36 -0300

    responsive 100%

--- js/scrollTo.js
+++ js/scrollTo.js
@@ -2,10 +2,10 @@ function scrollToHome(){
     window.scrollTo( 0, 0 );
 }
 function scrollToSobre(){
-    window.scrollTo( 0, 947 );
+    window.scrollTo( 0, 1035 );
 }
 function scrollToProjetos(){
-    window.scrollTo( 0, 1855 );
+    window.scrollTo( 0, 1923 );
 }
 function scrollToContato(){
     window.scrollTo( 0, 3400 );

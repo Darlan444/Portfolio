4607444a2cef48c24dc0fba63401defbee0ec4d2
Darlan444 <darlaninacio321@gmail.com>
2022-11-17 03:44:26 -0300

    close bug

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,4 @@
 function classMenu() {
     let el = document.getElementById('menuClass');
     el.classList.add('active');
-
-    let icon = document.getElementById('icon-menu')
-    icon.style.visibility="hidden";
 }

0b216ba77d522391d7251f140d8c683403f491af
Darlan444 <darlaninacio321@gmail.com>
2023-05-01 04:32:03 -0300

    att menu

--- js/scrollToMob.js
+++ js/scrollToMob.js
@@ -2,10 +2,10 @@ function scrollToHomeMob(){
     window.scrollTo( 0, 0 );
 }
 function scrollToSobreMob(){
-    window.scrollTo( 0, 807 );
+    window.scrollTo( 0, 900 );
 }
 function scrollToProjetosMob(){
-    window.scrollTo( 0, 1830 );
+    window.scrollTo( 0, 1920 );
 }
 function scrollToContatoMob(){
     window.scrollTo( 0, 5400 );
